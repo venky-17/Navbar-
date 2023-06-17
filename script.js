47da587d8cb8c1386502cd2a0adf6e66a3d811ce
Venkatesh K <128626606+venky-17@users.noreply.github.com>
2023-06-17 19:45:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const triggers = document.querySelectorAll('a')
+const highlight = document.createElement('span')
+
+highlight.classList.add('highlight')
+document.body.append(highlight)
+
+
+function highlightlink(){
+
+    const linkCords = this.getBoundingClientRect()
+   
+    highlight.style.width = `${linkCords.width}px`;
+    highlight.style.height = `${linkCords.height}px`;
+    highlight.style.transform = ` translate(${linkCords.left}px,${linkCords.top}px)`
+  
+     
+
+
+
+const coords = {
+    width: linkCords.width,
+    height: linkCords.height,
+    top: linkCords.top + window.scrollY,
+    left: linkCords.left+ window.scrollX
+}
+
+highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = ` translate(${coords.left}px,${coords.top}px)`
+  
+}
+
+triggers.forEach(a => a.addEventListener('mousemove', highlightlink))
